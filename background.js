f94231d65856e72e99386eceb962172b52d19c1b
Sachin Gupta <sachinvgupta9@gmail.com>
2024-06-23 23:04:31 -0400

    ss

--- background.js
+++ background.js
@@ -1,8 +1,32 @@
-﻿chrome.commands.onCommand.addListener((c) => {
+﻿function logIt() {
+  navigator.clipboard.writeText("hhhh");
+  navigator.clipboard.writeText("hiess").then(() => {
+    //clipboard successfully set
+    console.log("yyeyeshs");
+    chrome.tabs.create({url: "https://www.apple.com/"}, ()=>{});
+}, () => {
+    //clipboard write failed, use fallback
+});
+  
+
+}
+
+
+
+
+
+chrome.commands.onCommand.addListener((c) => {
   switch(c){
   case "command2":
     chrome.tabs.create({url: "https://www.google.com/"}, ()=>{});
-    chrome.storage.sync.set({ [2]: { text: "second", ishid: false } });
+    chrome.tabs.create({url: "https://www.apple.com/"}, ()=>{});
+    chrome.storage.sync.set({ [2]: { text: "secon", ishid: false } });
+    /*chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      func: logIt
+    })*/
+    logIt();
+    
     break;
   case "command3":
     chrome.tabs.create({url: "https://www.google.com/"}, ()=>{});
@@ -22,4 +46,3 @@
 
 
 
-function
