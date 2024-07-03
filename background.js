02ddca6d909419286adf6e0ef7c49d0175abcf2a
Sachin Gupta <sachinvgupta9@gmail.com>
2024-07-03 11:25:36 -0400

    the chrome extension application to google to get CoPeee on the app store got declined. After conducting research and troubleshooting adapting and rewriting the code to follow the rejection reason of unneccessary permissions, I have fixed it, and will now resubmit this version!!!

--- background.js
+++ background.js
@@ -24,6 +24,7 @@ chrome.commands.onCommand.addListener((c) => {
 })
 
 
+
 chrome.tabs.onActivated.addListener(function(tab) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { method: 'bgChangeColor' }, function (response) {
@@ -40,3 +41,58 @@ chrome.tabs.onUpdated.addListener(function(tab) {
     })
   })
 })
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function toSendMessage(theMethodName) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { method: theMethodName }, function (response) {
+        // to handle response
+    })
+  })
+}
+
+chrome.commands.onCommand.addListener((c) => {
+  switch(c){
+  case "command2":
+    toSendMessage('two')
+    break
+  case "command3":
+    toSendMessage('three')
+    break
+  case "command4":
+    toSendMessage('four')
+    break
+  case "command5":
+    toSendMessage('five')
+    break
+  }
+})
+
+
+chrome.tabs.onActivated.addListener(function(tab) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { method: 'bgChangeColor' }, function (response) {
+        // to handle response
+    })
+  })
+})
+
+
+chrome.tabs.onUpdated.addListener(function(tab) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { method: 'bgChangeColor' }, function (response) {
+        // to handle response
+    })
+  })
+})*/
